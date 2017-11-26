864ef9bc95fa79ce3d394482f76116b4400f141a
Thomas Swann <tkswann@mac.com>
2017-11-26 14:09:53 -0600

    added body-parser, created db connection middleware

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,17 @@
-// db connection file
+const r = require('rethinkdb');
+const { handleError } = require('../../utils');
+
+module.exports.connect = async (req, res, next) => {
+  try {
+    const connection = await r.connect({
+      db: 'test',
+      host: 'localhost',
+      port: process.env.DB_PORT
+    });
+    // add the db connection to the request object for easy access
+    req._rdb = connection;
+    next();
+  } catch (e) {
+    handleError(res)(e);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
-const mountRoutes = require('./routes');
+const db = require('./db');
+const { mountRoutes } = require('./routes');
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// create db connection and attach to req object
+app.use(db.connect);
+
+// mount async routes into main express app
 mountRoutes(app);
 
 app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
