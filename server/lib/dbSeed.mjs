0fa2b2e2e81f8e987ed343f1542fa380008240a9
Thomas Swann <tkswann@mac.com>
2017-12-05 18:45:52 -0600

    random user array func

--- server/lib/dbSeed.mjs
+++ server/lib/dbSeed.mjs
@@ -1,8 +1,15 @@
 import faker from 'faker';
 
-export const user = {
+export const user = () => ({
   email: faker.internet.email(),
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
   password: faker.internet.password()
-};
+});
+
+const userArray = length =>
+  Array(length)
+    .fill()
+    .map(() => user());
+
+console.dir(userArray(10));
